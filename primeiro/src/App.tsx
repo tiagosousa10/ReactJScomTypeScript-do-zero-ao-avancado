d166ae7f5658d42bcc145d99f598b7cf71e3947a
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-21 11:32:54 +0100

    exercicios com useStates

--- primeiro/src/App.tsx
+++ primeiro/src/App.tsx
@@ -1,16 +1,34 @@
 import React,{useState} from "react"
 
+interface InfoAlunoProps{
+  nome:string,
+  idade:string
+}
+
+
 export default function App(){
   const [input,setInput] = useState('')
-  const [aluno,setAluno] = useState('sem nada')
   const [idade,setIdade] = useState('')
 
+  const [infoAluno,setInfoAluno] = useState<InfoAlunoProps>()
+
+  const [contador,setContador] = useState(0)
+
   function mostrarAluno(){
-    setAluno(input)
-    console.log(idade)
+    setInfoAluno({
+      nome: input,
+      idade: idade,
+    })  
   }
 
+  function adicionar(){
+    setContador((valorAtual) => valorAtual + 1)
 
+  }
+
+  function diminuir(){
+    if(contador === 0) return;
+    setContador((valorAtual) => valorAtual - 1)  }
 
   return(
     <div>
@@ -18,12 +36,15 @@ export default function App(){
     <h1>Conhecendo useState</h1>
 
     <input 
-    type="text"
-     placeholder="Digite o nome" 
-     value={input}
-     onChange={(e) => setInput(e.target.value)} />
-  <br/>  <br/>
-     <input placeholder="Digite a idade" value={idade} onChange={(e) => setIdade(e.target.value)} />
+      type="text"
+      placeholder="Digite o nome" 
+      value={input}
+      onChange={(e) => setInput(e.target.value)} />
+    <br/>  <br/>
+     <input 
+      placeholder="Digite a idade" 
+      value={idade} 
+      onChange={(e) => setIdade(e.target.value)} />
 
     <br/>
 
@@ -31,7 +52,14 @@ export default function App(){
 
     <hr/>
 
-    <h3>Bem vindo {aluno}</h3>
+    <h3>Bem vindo: {infoAluno?.nome}</h3>
+    <h4>Idade  {infoAluno?.idade} </h4>
+
+    <hr/> 
+    <br/>
+
+    <h1>Contador com useState: </h1>
+    <button onClick={adicionar}>+</button> {contador} <button onClick={diminuir}>-</button>
     </div>
   )
 }
