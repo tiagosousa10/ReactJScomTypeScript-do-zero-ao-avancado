6b094685a240ce1953a6c13a78cb4996bdc285ac
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-23 18:26:02 +0100

    routes com outlet, header etc

---
+++ rotas/src/components/header/index.tsx
@@ -0,0 +1,15 @@
+import {Link } from 'react-router-dom'
+import './header.css'
+export function Header(){
+    return(
+        <header>
+            <h2>Sujeito Programador</h2>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/sobre">Sobre</Link>
+                <Link to="/contato">Contato</Link>
+
+            </div>
+        </header>
+    )
+}
--- rotas/src/pages/home/index.tsx
+++ rotas/src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import {Link} from 'react-router-dom'
 
+
 export function Home(){
     return(
         <div>
