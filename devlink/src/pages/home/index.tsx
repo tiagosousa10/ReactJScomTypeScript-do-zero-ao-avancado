a36809343bcaa1b8cd23cda4cd372dc400e3fb1d
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-25 00:13:27 +0100

    adicionando pagHome + inicio do login

--- devlink/src/pages/home/index.tsx
+++ devlink/src/pages/home/index.tsx
@@ -1,7 +1,36 @@
+import {Social} from '../../components/social'
+import { FaFacebook,FaInstagram,FaYoutube} from 'react-icons/fa'
+
 export function Home(){
     return(
-        <div className="w-full h-screen bg-neutral-950">
-            <h1 className="text-white text-3xl">pagina HOME</h1>
+        <div className="flex flex-col w-full py-4 items-center justify-center" >
+            <h1 className="md:text-4lx text-3xl font-bold text-white mt-20">Tiago Sousa</h1>
+            <span className="text-gray-50 mb-5 mt-3">Veja os meus Links! 👇</span>
+
+            <main className="flex flex-col w-11/12 max-w-xl text-center">
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+                    <a>
+                        <p className="text-base md:text-lg">
+                            Canal no Youtube
+                        </p>
+                    </a>
+                </section>
+
+
+                <footer className="flex justify-center gap-3 my-4">
+                    <Social url="https://facebook.com/">
+                    <FaFacebook size={35} color='#fff'/>
+                    </Social>
+
+                    <Social url="https://youtube.com/">
+                    <FaYoutube size={35} color='#fff'/>
+                    </Social>
+
+                    <Social url="https://instagram.com/">
+                    <FaInstagram size={35} color='#fff'/>
+                    </Social>
+                </footer>
+            </main>
         </div>
     )
 }
