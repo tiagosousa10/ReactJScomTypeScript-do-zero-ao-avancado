1a1f669a36ff613ef548b8139da4bf7bdfb891a1
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-24 01:21:30 +0100

    começando a colocar os componentes da pag Home

--- criptoapp/src/pages/home/index.tsx
+++ criptoapp/src/pages/home/index.tsx
@@ -1,9 +1,61 @@
+import styles from './home.module.css'
+import {BsSearch} from 'react-icons/bs'
+import { Link} from 'react-router-dom'
+
 function Home() {
 
     return (
-      <div>
-        <h1>Pagina HOME</h1>
-      </div>
+      <main className={styles.container}>
+
+        <form action="" className={styles.form}>
+          <input type="text" placeholder='Digite o nome da moeda... ex. Bitcoin' />
+          <button type='submit'>
+            <BsSearch size={30} color='#fff' />
+          </button>
+        </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope='col'>Moeda</th>
+            <th scope='col'>Valor de Mercado</th>
+            <th scope='col'>Preço</th>
+            <th scope='col'>Volume</th>
+            <th scope='col'>Mudança 24h</th>
+          </tr>
+        </thead>
+        <tbody  id='tbody'>
+          <tr className={styles.tr} >
+
+            <td className={styles.tdLabel} data-label="Moeda">
+              <div className={styles.name}>
+              <Link to={'/detail/bitcoin'}>
+                <span>Bitcoin</span> | BTC
+              </Link>
+              </div>             
+            </td>
+
+            <td className={styles.tdLabel} data-label="Valor de Mercado">
+              1T
+            </td>
+
+            <td className={styles.tdLabel} data-label="Preço">
+              8.000
+            </td>
+
+            <td className={styles.tdLabel} data-label="Volume">
+              2B
+            </td>
+
+            <td className={styles.tdLabel} data-label="Mudança 24h">
+              <span>1.20</span>
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
+
+      </main>
     )
   }
   
--- criptoapp/src/router.tsx
+++ criptoapp/src/router.tsx
@@ -3,9 +3,11 @@ import {createBrowserRouter } from 'react-router-dom';
 import Home from './pages/home'
 import Detail from './pages/detail'
 import NotFound from './pages/notfound'
+import { Layout } from './components/layout';
 
 const router  = createBrowserRouter([
     {
+        element: <Layout/>,
         children:[
             {
                 path:"/",
