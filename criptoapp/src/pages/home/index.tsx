3339485c51e4ee94460da279db655f428345455b
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-24 02:07:25 +0100

    fetch da api mais formatacao dos dados dos objetos

--- criptoapp/src/pages/home/index.tsx
+++ criptoapp/src/pages/home/index.tsx
@@ -1,19 +1,89 @@
+import {useState, FormEvent, useEffect} from 'react'
 import styles from './home.module.css'
 import {BsSearch} from 'react-icons/bs'
-import { Link} from 'react-router-dom'
+import { Link, useNavigate} from 'react-router-dom'
+
+interface CoinProps{
+  id:string;
+  name:string;
+  symbol:string;
+  priceUsd:string;
+  vwap24Hr:string;
+  changePercent24Hr:string;
+  rank:string;
+  supply:string;
+  maxSupply:string;
+  marketCapUsd:string;
+  volumeUsd24Hr:string;
+  explorer:string;
+}
+
+interface DataProps{
+  data: CoinProps[]
+}
 
 function Home() {
+  const [input,setInput] = useState('')
+  const [coins,setCoins] = useState<CoinProps[]>([])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getData()
+  },[])
+
+  async function getData(){
+    fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+    .then((response) => response.json()).then((data : DataProps) => {
+      const coinsData = data.data;
+
+      const price = Intl.NumberFormat("en-US", {
+        style:'currency',
+        currency:'USD'
+      })
+
+      const formatedResult = coinsData.map((item) => {
+        const formated = {
+          ...item,
+          formatedPrice: price.format(Number(item.priceUsd)),
+          formatedMarket : price.format(Number(item.marketCapUsd))
+        }
+        return formated
+      })
+      console.log(formatedResult)
+    })
+  }
+
+  function handleSubmit(e:FormEvent){
+    e.preventDefault()
+
+    if(input === '') return;
+
+    navigate(`/detail/${input}`)
+  }
+
+  function handleGetMore(){
+    alert('testes')
+  }
+
+
 
     return (
       <main className={styles.container}>
 
-        <form action="" className={styles.form}>
-          <input type="text" placeholder='Digite o nome da moeda... ex. Bitcoin' />
+        <form action="" className={styles.form} onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            placeholder='Digite o nome da moeda... ex. Bitcoin'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+           />
           <button type='submit'>
             <BsSearch size={30} color='#fff' />
           </button>
         </form>
 
+{/* ---TABELA--- */}
       <table>
         <thead>
           <tr>
@@ -25,6 +95,7 @@ function Home() {
           </tr>
         </thead>
         <tbody  id='tbody'>
+
           <tr className={styles.tr} >
 
             <td className={styles.tdLabel} data-label="Moeda">
@@ -47,14 +118,18 @@ function Home() {
               2B
             </td>
 
-            <td className={styles.tdLabel} data-label="Mudança 24h">
+            <td className={styles.tdProfit} data-label="Mudança 24h">
               <span>1.20</span>
             </td>
 
           </tr>
+
         </tbody>
       </table>
 
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais
+      </button>
       </main>
     )
   }
