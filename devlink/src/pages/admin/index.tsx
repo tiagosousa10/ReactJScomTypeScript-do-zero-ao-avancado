40d2475bee3ed98f1e143b4a826cd026dff840d1
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-25 14:33:05 +0100

    adicionando dados na base dados - inputs dinamicos com firebase

--- devlink/src/pages/admin/index.tsx
+++ devlink/src/pages/admin/index.tsx
@@ -1,17 +1,64 @@
-import {useState} from 'react'
+import {FormEvent, useState} from 'react'
+import {FiTrash} from 'react-icons/fi'
 
 import { Header } from "../../components/Header";
 import {Input} from '../../components/Input'
+
+import {db} from '../../services/firebaseConnection'
+import {
+    addDoc,
+    collection,
+    onSnapshot,
+    query,
+    orderBy,
+    doc,
+    deleteDoc,
+} from 'firebase/firestore'
+
+
+
 export function Admin(){
     const [nameInput,setNameInput] = useState('')
     const [urlInput,setUrlInput] = useState('')
     const [textColorInput,setTextColorInput] = useState("#f1f1f1")
     const [backgroundColorInput,setBackgroundColorInput] = useState("#121212")
 
+
+
+    function handleRegister(e:FormEvent){
+        e.preventDefault()
+        
+        if(nameInput === '' || urlInput === ''){
+            alert('Preencha todos os campos!') 
+            return;
+        }
+
+        //criar collection
+        addDoc(collection(db,"links"), {
+            name:nameInput,
+            url:urlInput,
+            bg:backgroundColorInput,
+            color:textColorInput,
+            created:new Date()
+        }).then(() => {
+            setNameInput('')
+            setUrlInput('')
+            console.log('cadastrado com sucesso!')
+        }).catch((e) => {
+            console.log("erro ao cadastrar. " + e)
+        })
+
+
+    }
+
+
+
     return(
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header/>
-            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+                                                {/* FORMULARIO  */}
+
+            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl" onSubmit={handleRegister}>
                 <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
                 <Input 
                     placeholder="Digite o nome do link..."
@@ -47,18 +94,47 @@ export function Admin(){
 
                 </section>    
 
-                <div className='flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md'>
-                <label className="text-white font-medium mt-2 mb-3">Veja como está ficando:</label>
-                    <article 
-                    className='w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3'
-                    style={{marginBottom:8, marginTop:8, backgroundColor: backgroundColorInput}}
-                    >
-                        <p 
-                        className='font-medium'
-                        style={{color:textColorInput}}>Canal do Youtube</p>
-                    </article>
-                </div>   
+               {nameInput !== '' && (
+                 <div className='flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md'>
+                 <label className="text-white font-medium mt-2 mb-3">Veja como está ficando:</label>
+                     <article 
+                     className='w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3'
+                     style={{marginBottom:8, marginTop:8, backgroundColor: backgroundColorInput}}
+                     >
+                         <p 
+                         className='font-medium'
+                         style={{color:textColorInput}}>{nameInput}</p>
+                     </article>
+                 </div>   
+               )}
+
+                <button 
+                type='submit'
+                className='bg-blue-600 mb-7 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center'>
+                    Cadastrar
+                </button>
+
+
             </form>
+
+                                                {/* MEUS LINKS  */}
+            <h2 className='font-bold text-white mb-4 text-2xl'>Meus Links</h2>
+
+            <article
+             className='flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none'
+             style={{backgroundColor:"#2563eb", color:"#FFF"}}
+             >
+                <p>Canal do Youtube</p>
+                <div>
+                    <button
+                    className='border border-dashed p-1 rounded bg-neutral-900'
+                    >
+                        <FiTrash size={18} color='#fff'/>
+                    </button>
+                </div>
+            </article>
+
+
         </div>
     )
 }
