ed574d3192a7660c251d7b7ebd89a7c03ac6b56d
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-26 16:10:27 +0100

    adicionando page 404, e regras de acesso na firebase

---
+++ devlink/src/pages/error/index.tsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+export function ErrorPage(){
+    return(
+        <div className='flex w-full min-h-screen justify-center items-center flex-col text-white'>
+            <h1 className='font-bold text-6xl mb-2'>404</h1>
+            <h1 className='font-bold text-4xl mb-4 '>Pagina não encontrada!</h1>
+            <p className='italic text-1xl mb-4'>Esta página não existe!</p>
+        <Link to={'/'} className='bg-gray-50/20 py-1 px-4 rounded-md'>Voltar para Home</Link>
+        </div>
+    )
+}
