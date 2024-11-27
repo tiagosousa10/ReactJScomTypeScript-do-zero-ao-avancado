6c19b381ff3896213a31823b25f0f9176db6a73f
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-27 23:16:37 +0100

    comunicaçao com api + requisicao para mostrar os dados

--- carrinho/src/pages/Home/index.tsx
+++ carrinho/src/pages/Home/index.tsx
@@ -1,22 +1,50 @@
+import {useState,useEffect} from 'react'
 import {BsCartPlus} from 'react-icons/bs'
 
+import { api } from '../../services/api'
+
+interface ProductProps{
+    id:number,
+    title:string,
+    description:string,
+    price:number,
+    cover:string
+}
+
 export function Home(){
+    const [products,setProducts] = useState<ProductProps[]>([])
+
+    useEffect(() => {
+        async function getProducts(){
+            const response = await api.get('/products')
+            setProducts(response.data)
+        }
+
+        getProducts()
+    },[])
+    
+
+
     return(
         <div>
             <main className="w-full max-w-7xl px-4 mx-auto ">
                 <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
 
                 <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5'>
-                    <section className="w-full">
+                    {products.map((product) => (
+                        <section className="w-full" key={product.id}>
                         <img 
                         className='w-full rounded-lg max-h-70 mb-2'
-                        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsupport.apple.com%2Flibrary%2FAPPLE%2FAPPLECARE_ALLGEOS%2FSP750%2FSP750-airpods.jpeg&f=1&nofb=1&ipt=bc20df1e7c121650ed53e88ad48c9f8960ef9a8d7fea89bd199da805741187d9&ipo=images"
-                        alt="logoDoProduto"   />
-                        <p className='font-medium mt-1 mb-2'>Airpods pro</p>
+                        src={product.cover}
+                        alt={product.title}   />
+                        <p className='font-medium mt-1 mb-2'>{product.title}</p>
 
                         <div className='flex gap-3 items-center'>
                             <strong className='text-zinc-700/90'>
-                                1000 $
+                                {product.price.toLocaleString('pt-PT',{
+                                    style:"currency",
+                                    currency:"EUR"
+                                })}
                             </strong>
                             <button className='bg-zinc-900 p-1 rounded'>
                                 <BsCartPlus size={20} color='#fff'    />
@@ -24,6 +52,7 @@ export function Home(){
                         </div>
 
                     </section>
+                    ))}
 
 
                    
