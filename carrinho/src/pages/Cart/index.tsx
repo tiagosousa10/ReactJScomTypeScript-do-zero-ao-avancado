0a4bf901b4f8916a25d7384ba2a4589d0b4e0b5d
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-27 23:02:25 +0100

    criando page Carrinho de Compras

--- carrinho/src/pages/Cart/index.tsx
+++ carrinho/src/pages/Cart/index.tsx
@@ -1,7 +1,38 @@
 export function Cart(){
     return(
-        <div>
-            <h1>Pagina Cart</h1>
+        <div className="w-full max-w-7xl mx-auto ">
+            <h1 className="font-medium text-2xl text-center my-4">Carrinho de Compras</h1>
+
+            <section className="flex items-center justify-between border-b-2 border-gray-300">
+                <img 
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsupport.apple.com%2Flibrary%2FAPPLE%2FAPPLECARE_ALLGEOS%2FSP750%2FSP750-airpods.jpeg&f=1&nofb=1&ipt=bc20df1e7c121650ed53e88ad48c9f8960ef9a8d7fea89bd199da805741187d9&ipo=images"
+                    alt="logoProduto"
+                    className="w-28"
+                   />
+
+                   <strong>Preço: 1000$</strong>
+
+
+                   <div className="flex items-center justify-center gap-3">
+                    <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                        -
+                    </button>
+
+                        1
+
+                    <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                        +
+                    </button>
+                   </div>
+
+
+                   <strong className="float-right">
+                    SubTotal: 1000$
+                   </strong>
+            </section>
+
+
+            <p className="font-bold mt-4">Total: 1000$</p>
         </div>
     )
 }
