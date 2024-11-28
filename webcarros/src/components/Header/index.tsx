ea5b9adb4e42bb80ad6a1b8ecb4c04fed8a287a7
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-28 21:57:34 +0100

    construindo pageHome - grid para Carros

--- webcarros/src/components/Header/index.tsx
+++ webcarros/src/components/Header/index.tsx
@@ -1,7 +1,36 @@
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg'
+import { FiUser,FiLogIn} from 'react-icons/fi'
+
 export function Header(){
+    const signed = true;
+    const loadingAuth = false;
+
     return(
-        <div>
-            <h1>HEADER!</h1>
+        <div className='w-full flex  items-center justify-center h-16 bg-white drop-shadow mb-4'>
+            <header className='flex w-full items-center max-w-7xl justify-between px-4 mx-auto'>
+
+                <Link to={'/'}>
+                <img src={logoImg} alt="logoSite" />
+                </Link>
+
+              {!loadingAuth && signed && (
+                  <Link to={'/dashboard'}>
+                  <div className='border-2 rounded-full p-1 border-gray-900'>
+                  <FiUser size={24} color='#000'    />
+                  </div>
+                  </Link>
+              )}
+
+              {!loadingAuth && !signed && (
+                  <Link to={'/login'}>
+                 <div className='border-2 rounded-full p-1 border-gray-900'>
+                  <FiLogIn size={24} color='#000'    />
+                  </div>
+                  </Link>
+              )}
+
+            </header>
         </div>
     )
 }
--- webcarros/src/pages/Home/index.tsx
+++ webcarros/src/pages/Home/index.tsx
@@ -1,5 +1,42 @@
+import { Container } from "../../components/Container";
+
 export function Home(){
     return(
-        <h1>Pagina - HOME</h1>
+        <Container>
+        <section className="bg-white p-4 rounde-lg w-full max-w-3xl mx-auto flex items-center gap-2">
+            <input 
+                type="text" 
+                placeholder="Digite o nome do carro..." className="w-full border-2 rounde-lg h-9 px-3 outline-none" />
+            <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+                Pesquisar
+            </button>
+        </section>
+
+        <h1 className="font-bold text-center mt-6 text-2xl mb-4">Carros novos e usados em Portugal</h1>
+
+        <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            <section className="w-full bg-white rounded-lg" >
+                <img 
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.motor1.com%2Fimages%2Fmgl%2F8LQ6M%2Fs1%2Flamborghini-aventador-svj-63-roadster.jpg&f=1&nofb=1&ipt=c1dfa089666a7b318e64282e6ab73a2568181f01245ce65558dea19baa761b24&ipo=images"
+                    alt="nomeCarro"
+                    className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                   />
+                <p className="font-bold mt-1 mb-2 px-2">Lamborghini Aventador</p>   
+                <div className="flex flex-col px-2">
+                    <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.00 km</span>
+                    <strong className="text-black font-medium text-xl">235.000$</strong>
+                </div>
+
+                <div className="w-full h-px bg-slate-200 my-2"></div>
+
+                <div className="px-2 pb-2">
+                    <span className="text-black">Porto - Portugal</span>
+                </div>
+            </section>
+
+            
+        </main>
+
+        </Container>
     )
 }
