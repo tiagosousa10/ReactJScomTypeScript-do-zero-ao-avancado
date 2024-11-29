0f7f19c44f76612890e463b9c583afc00d13a9c8
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-29 15:07:02 +0100

    comecando o formulario para addCar

--- webcarros/src/components/PanelHeader/index.tsx
+++ webcarros/src/components/PanelHeader/index.tsx
@@ -11,7 +11,7 @@ export function PanelHeader(){
     }
 
     return(
-        <div className='w-full items-center flex h-10 bg-red-500 rounded-lg text-white font-medium gap-4 px-4'>
+        <div className='w-full items-center flex h-10 bg-red-500 rounded-lg text-white font-medium gap-4 px-4 mb-4'>
             <Link to={'/dashboard'}>Dashboard</Link>
             <Link to={'/dashboard/new'}>Adicionar Carro</Link>
 
--- webcarros/src/pages/Dashboard/New/index.tsx
+++ webcarros/src/pages/Dashboard/New/index.tsx
@@ -1,5 +1,29 @@
+import { Container } from "../../../components/Container";
+import { PanelHeader } from "../../../components/PanelHeader";
+
+import {FiUpload} from 'react-icons/fi'
+
 export function New(){
     return(
-        <h1>Pagina NewCarro!</h1>
+        <Container>
+            <PanelHeader/>
+
+            <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+                <button 
+                    className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+                    <div className="absolute cursor-pointer">
+                        <FiUpload size={30} color="#000"  />
+                    </div>
+                    <div className="cursor-pointer">
+                        <input type="file" accept="image/**" className="opacity-0 cursor-pointer" />
+                    </div>
+                </button>
+            </div>
+
+            <div className="w-full bg-white p-3 rounded-lg flex-col flex sm:flex-row items-center gap-2 mt-2">
+                <h1>TESTE</h1>
+            </div>
+
+        </Container>
     )
 }
