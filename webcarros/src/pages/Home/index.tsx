147f9908d67ced63d72e2abbf4176748e9280560
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-30 13:58:25 +0100

    page Home, aceder aos carros criados + mapear os campos

--- webcarros/src/pages/Home/index.tsx
+++ webcarros/src/pages/Home/index.tsx
@@ -1,6 +1,60 @@
+import {useState,useEffect} from 'react'
 import { Container } from "../../components/Container";
+import {collection,query,getDocs,orderBy} from 'firebase/firestore'
+import {db} from '../../services/firebaseConnection'
+import { Link } from 'react-router-dom';
+
+
+interface CarProps{
+    id:string,
+    name:string,
+    year:string,
+    uid:string,
+    price:string | number,
+    city:string,
+    km:string,
+    images: CarImageProps[]
+}
+
+interface CarImageProps{
+    name:string,
+    uid:string,
+    url:string
+}
 
 export function Home(){
+    const [ cars,setCars] = useState<CarProps[]>([])
+
+    useEffect(() => {
+        function loadCars(){
+            const carsRef = collection(db,'cars')
+            const queryRef = query(carsRef, orderBy('created', 'desc'))
+        
+            getDocs(queryRef)
+            .then((snapshot) => {
+                const listCars = [] as CarProps[]
+
+                snapshot.forEach((doc) => {
+                    listCars.push({
+                        id:doc.id,
+                        name:doc.data().name,
+                        year:doc.data().year,
+                        km:doc.data().km,
+                        city:doc.data().city,
+                        price:doc.data().price,
+                        images:doc.data().images,
+                        uid: doc.data().uid
+                    })
+                })
+
+                setCars(listCars)
+            })
+        }
+
+        loadCars()
+    }, [])
+
+
     return(
         <Container>
         <section className="bg-white p-4 rounde-lg w-full max-w-3xl mx-auto flex items-center gap-2">
@@ -15,25 +69,29 @@ export function Home(){
         <h1 className="font-bold text-center mt-6 text-2xl mb-4">Carros novos e usados em Portugal</h1>
 
         <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            <section className="w-full bg-white rounded-lg" >
-                <img 
-                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.motor1.com%2Fimages%2Fmgl%2F8LQ6M%2Fs1%2Flamborghini-aventador-svj-63-roadster.jpg&f=1&nofb=1&ipt=c1dfa089666a7b318e64282e6ab73a2568181f01245ce65558dea19baa761b24&ipo=images"
-                    alt="nomeCarro"
-                    className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                   />
-                <p className="font-bold mt-1 mb-2 px-2">Lamborghini Aventador</p>   
-                <div className="flex flex-col px-2">
-                    <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.00 km</span>
-                    <strong className="text-black font-medium text-xl">235.000$</strong>
-                </div>
 
-                <div className="w-full h-px bg-slate-200 my-2"></div>
+            {cars.map((car) => (
+                <Link key={car.id} to={`/car/${car.id}`}>
+                    <section  className="w-full bg-white rounded-lg" >
+                    <img 
+                        src={car.images[0].url}
+                        alt="nomeCarro"
+                        className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                    />
+                    <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>   
+                    <div className="flex flex-col px-2">
+                        <span className="text-zinc-700 mb-6">{car.year} |{car.km}</span>
+                        <strong className="text-black font-medium text-xl">{car.price} $</strong>
+                    </div>
 
-                <div className="px-2 pb-2">
-                    <span className="text-black">Porto - Portugal</span>
-                </div>
-            </section>
+                    <div className="w-full h-px bg-slate-200 my-2"></div>
 
+                    <div className="px-2 pb-2">
+                        <span className="text-black">{car.city}</span>
+                    </div>
+                </section>
+            </Link>
+            ))}
             
         </main>
 
