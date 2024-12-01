edebd0b49accfe90bfc70634596d865f503ff4a2
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-01 14:13:34 +0100

    criando slider

--- webcarros/src/main.tsx
+++ webcarros/src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import {router} from './App'
+
 import { RouterProvider} from 'react-router-dom'
 import AuthProvider from './contexts/AuthContext'
+
+import {register} from 'swiper/element/bundle'
+register()
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 
 createRoot(document.getElementById('root')!).render(
--- webcarros/src/pages/Car/index.tsx
+++ webcarros/src/pages/Car/index.tsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom'
 import {getDoc,doc} from 'firebase/firestore'
 import {db} from '../../services/firebaseConnection'
 
+import {Swiper,SwiperSlide} from 'swiper/react'
 
 interface CarProps{
     id:string,
@@ -32,6 +33,7 @@ interface ImagesCarProps{
 export function CarDetail(){
     const {id} = useParams()
     const [car,setCar]= useState<CarProps>()
+    const [sliderPerView,setSliderPerView] = useState<number>(2)
 
     useEffect(() => {
         async function loadCar(){
@@ -63,10 +65,38 @@ export function CarDetail(){
 
         console.log(car)
     },[id])
-        
+      
+    
+    useEffect(() => {
+        function handleResize(){
+            if(window.innerWidth < 720){
+                setSliderPerView(1)
+            } else {
+                setSliderPerView(2)
+            }
+        }
+
+        handleResize()
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+
+    },[])
+
+
     return(
         <Container>
-        <h1>SLIDER </h1>
+        <Swiper slidesPerView={sliderPerView} pagination={{clickable:true}} navigation>
+            {car?.images.map((image) => (
+                <SwiperSlide key={image.name}>
+
+                    <img  src={image.url} className='w-full h-96 object-cover' />
+                </SwiperSlide>
+            ))}
+        </Swiper>
 
         {car && (
             <main className='w-full bg-white rounded-lg p-6 my-4'>
